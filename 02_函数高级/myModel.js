420942f2d696a5a151205df94311eeb8055eedc9
yexinwei404 <101961837+yexinwei404@users.noreply.github.com>
2022-06-08 09:35:27 +0800

    master

---
+++ 02_函数高级/myModel.js
@@ -0,0 +1,16 @@
+function myModel(){
+    // 私有数据
+    var msg = 'My atguigu'
+    // 操作数据的函数 
+    function doSomething(){
+        console.log('doSomething()' + msg.toLocaleUpperCase())
+    }
+    function doOtherthing () {
+        console.log('doOtherthing()' + msg.toLocaleLowerCase())
+    }
+    // 向外暴露对象(给外部使用的方法)
+    return {
+        a:doSomething,
+        b:doOtherthing
+    }
+}
